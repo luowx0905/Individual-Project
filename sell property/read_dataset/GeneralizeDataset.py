fcdc922194449ccc7efeb7ddeac8d5cb1c6c30d7
slywl3 <slywl3@163.com>
2022-06-28 14:57:09 +0100

    Add unit test for generalize dataset

--- sell property/read_dataset/GeneralizeDataset.py
+++ sell property/read_dataset/GeneralizeDataset.py
@@ -9,10 +9,13 @@ class GeneralizeDataset:
         self.parking_types = None
         self.parking_num = None
 
+        self.outside_space_types = None
+        self.outside_space_num = None
+
     def get_parking_types(self):
         # Select all the columns related to parking spaces
-        parking_name = list(filter(lambda s: "parking" in s, data.columns))
-        parking = data[parking_name].cpoy()
+        parking_name = list(filter(lambda s: "parking" in s, self.data.columns))
+        parking = self.data[parking_name].copy()
 
         # Fill all the missing values to None
         parking = parking.fillna("None")
@@ -23,33 +26,88 @@ class GeneralizeDataset:
 
         # Obtain parking types for every property
         for i in range(len(parking)):
+            types = {key: 0 for key in types if key != "None"}
+
             first = parking.iloc[i, 0]
             second = parking.iloc[i, 1]
             third = parking.iloc[i, 2]
-            types = self.get_parking_spaces_for_property(first, second, third)
+            types = self.record_types(first, second, third, types)
 
-            list(map(lambda value, d: self.parking_types[d].append(types[value]),
-                     types.keys(), types.keys()))
+            list(map(lambda d: self.parking_types[d].append(types[d]), types.keys()))
 
     def get_parking_num(self):
-        parking_name = list(filter(lambda s: "parking" in s, data.columns))
-        parking = data[parking_name]
+        parking_name = list(filter(lambda s: "parking" in s, self.data.columns))
+        parking = self.data[parking_name]
 
         self.parking_num = [parking.iloc[i].count() for i in range(len(parking))]
 
+    def get_outside_space_types(self):
+        outside_name = list(filter(lambda s: "outside_space" in s, self.data.columns))
+        outside = self.data[outside_name].copy()
+
+        outside = outside.fillna("None")
+        types, counts = np.unique(outside.to_numpy().flatten(), return_counts=True)
+
+        self.outside_space_types = {key: [] for key in types if key != "None"}
+
+        for i in range(len(outside)):
+            types = {key: 0 for key in types if key != "None"}
+
+            first = outside.iloc[i, 0]
+            second = outside.iloc[i, 1]
+            third = outside.iloc[i, 2]
+            types = self.record_types(first, second, third, types)
+
+            list(map(lambda d: self.outside_space_types[d].append(types[d]), types.keys()))
+
+    def get_feature_types(self, feature: str):
+        """
+        Obtain feature types for every property
+        :param feature: parking, outside_space
+        :return: None
+        """
+        names = list(filter(lambda s: feature in s, self.data.columns))
+        raw_data = self.data[names].copy()
+
+        raw_data = raw_data.fillna("None")
+        types = np.unique(raw_data.to_numpy().flatten())
+
+        result = None
+        if feature.lower() == "parking":
+            self.parking_types = {key: [] for key in types if key != "None"}
+            result = self.parking_types
+        elif feature.lower() == "outside_space":
+            self.outside_space_types = {key: [] for key in types if key != "None"}
+            result = self.outside_space_types
+        else:
+            raise ValueError("{} is invalid".format(feature))
+
+        for i in range(len(raw_data)):
+            types = {key: 0 for key in types if key != "None"}
+
+            first = raw_data.iloc[i, 0]
+            second = raw_data.iloc[i, 1]
+            third = raw_data.iloc[i, 2]
+            types = self.record_types(first, second, third, types)
+
+            list(map(lambda d: result[d].append(types[d]), types.keys()))
+
+    def get_outside_space_num(self):
+        outside_name = list(filter(lambda s: "outside_space" in s, self.data.columns))
+        outside = self.data[outside_name]
+
+        self.outside_space_num = [outside.iloc[i].count() for i in range(len(outside))]
+
     @staticmethod
-    def get_parking_spaces_for_property(first: str, second: str, third: str):
-        parking_types = {"Allocated": 0, "Off Street": 0, "Residents": 0, "On Street": 0, "Driveway": 0, "Garage": 0,
-                         "Permit": 0, "Private": 0, "Gated": 0, "Covered": 0, "Communal": 0, "Rear": 0}
+    def record_types(first: str, second: str, third: str, types: dict):
+        if first in types.keys():
+            types[first] += 1
+        if second in types.keys():
+            types[second] += 1
+        if third in types.keys():
+            types[third] += 1
 
-        if first in parking_types.keys():
-            parking_types[first] += 1
-        if second in parking_types.keys():
-            parking_types[second] += 1
-        if third in parking_types.keys():
-            parking_types[third] += 1
-
-        return parking_types
+        return types
 
 
 if __name__ == '__main__':
@@ -57,6 +115,6 @@ if __name__ == '__main__':
     data = pd.read_csv(filename, encoding="ISO8859-1")
     generalize = GeneralizeDataset(data)
 
-    generalize.get_parking_num()
-    for i, num in enumerate(generalize.parking_num):
-        print("{:5d}{:5d}".format(i, num))
+    generalize.get_outside_space_num()
+    for k, v in enumerate(generalize.outside_space_num):
+        print("{:15d}{:5d}".format(k, v))
---
+++ sell property/read_dataset/TestGeneralizeDataset.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+from GeneralizeDataset import GeneralizeDataset
+import pandas as pd
+import unittest
+
+
+class TestGeneralizeDataset(TestCase):
+    filename = "../datasets/PropertyData_wDesc.csv"
+    data = pd.read_csv(filename, encoding="ISO8859-1")
+
+    def test_get_parking_types(self):
+        generalize = GeneralizeDataset(self.data)
+        generalize.get_feature_types("parking")
+
+        result = {"Allocated": 343, "Communal": 81, "Covered": 36, "Driveway": 1257, "Garage": 842,
+                  "Gated": 89, "Off Street": 1209, "On Street": 458, "Permit": 70, "Private": 258,
+                  "Rear": 27, "Residents": 90}
+
+        self.assertEqual(len(generalize.parking_types), 12)
+        for k in generalize.parking_types.keys():
+            self.assertEqual(sum(generalize.parking_types[k]), result[k])
+
+    def test_get_parking_num(self):
+        generalize = GeneralizeDataset(self.data)
+        generalize.get_parking_num()
+
+        self.assertEqual(sum(generalize.parking_num), 4760)
+
+    def test_get_outside_space_types(self):
+        generalize = GeneralizeDataset(self.data)
+        generalize.get_feature_types("outside_space")
+
+        result = {"Back Garden": 1366, "Communal Garden": 227, "Enclosed Garden": 735, "Front Garden": 682,
+                  "Patio": 431, "Private Garden": 599, "Rear Garden": 802, "Terrace": 93}
+
+        self.assertEqual(len(generalize.outside_space_types), 8)
+        for k in generalize.outside_space_types.keys():
+            self.assertEqual(sum(generalize.outside_space_types[k]), result[k])
+
+    def test_get_outside_space_num(self):
+        generalize = GeneralizeDataset(self.data)
+        generalize.get_outside_space_num()
+
+        self.assertEqual(sum(generalize.outside_space_num), 4935)
+
+
+if __name__ == '__main__':
+    unittest.main()
